e142380afd4a0518035183cf42fdd036765b61ed
AlexandraMihalevschi <68810700+AlexandraMihalevschi@users.noreply.github.com>
2021-12-06 12:19:22 +0300

    Add files via upload

---
+++ Evaluare sumativă fișiere.py
@@ -0,0 +1,27 @@
+with open("C:/Users/Alexandra/Desktop/Python/INPUT.txt", 'r') as f:
+    print("Nr Numele Prenumele Nota1 Nota2 Nota3")
+    print(f.read())
+with open("C:/Users/Alexandra/Desktop/Python/INPUT.txt", 'r') as f:
+    tx = list(f)
+
+
+with open("C:/Users/Alexandra/Desktop/Python/INPUT.txt", 'r') as f:
+    with open('REZERVA.txt', 'w') as r:
+        for i in f:
+            r.write(i)    
+
+for i in tx:
+    c = i.split()
+    nota1 = float(c[3])
+    nota2 = float(c[4])
+    nota3 = float(c[5])
+    medie = (nota1 + nota2 + nota3)/3
+    with open('OUTPUT.txt', 'a') as g:
+        g.write(c[0])
+        g.write(' ')
+        g.write(c[1])
+        g.write(' ')
+        g.write(c[2])
+        g.write(' ')
+        g.write(str(medie))
+        g.write('\n')
